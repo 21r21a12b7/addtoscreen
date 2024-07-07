41a324c38bdc4a8befffb59b479034af2c899638
21r21a12b7 <21r21a12b7@mlrinstitutions.ac.in>
2024-07-07 23:44:03 +0530

    first commit

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service worker installed');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    // You can add custom fetch event handling here if needed
-  });
-  
